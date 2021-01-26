b3ca2faddc9ac439792f6a664c4c5888e781e4a1
Davinder Singh <dvndrsingh786@gmail.com>
2021-01-26 10:06:55 +0530

    Initial Commit

---
+++ Ludo Olympic Local/Assets/Dav Obj/_Scripts/TempGameManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using AssemblyCSharp;
+
+public class TempGameManager : MonoBehaviour
+{
+    public bool lostFocus;
+    public static TempGameManager tempGM;
+    public List<int> theIndex;
+    public List<bool> theIndexVal;
+    public PhotonView view;
+    public GameGUIController controller;
+    public bool iamalive = true;
+
+    void Start()
+    {
+        PhotonNetwork.BackgroundTimeout = StaticStrings.photonDisconnectTimeoutLong;
+        tempGM = this;
+
+        if (GameManager.Instance.type == MyGameType.Private)
+        {
+            ReferenceManager.refMngr.ChangeWinningAmountManually(GameManager.Instance.currentBetAmount, PhotonNetwork.room.PlayerCount);
+        }
+
+        if (PhotonNetwork.inRoom)
+        {
+            if (PhotonNetwork.room.CustomProperties != null)
+            {
+                if (PhotonNetwork.room.CustomProperties.ContainsKey("bt"))
+                {
+                    GameManager.Instance.doesContainBotMoves = true;
+                }
+            }
+        }
+        Invoke(nameof(SetTimerNames), 0.8f);
+    }
+
+    void SetTimerNames()
+    {
+        GameGUIController contrlr = FindObjectOfType<GameGUIController>();
+        UpdatePlayerTimer timr1 = contrlr.PlayersTimers[0].GetComponent<UpdatePlayerTimer>();
+        UpdatePlayerTimer timr2 = contrlr.PlayersTimers[1].GetComponent<UpdatePlayerTimer>();
+        UpdatePlayerTimer timr3 = contrlr.PlayersTimers[2].GetComponent<UpdatePlayerTimer>();
+        UpdatePlayerTimer timr4 = contrlr.PlayersTimers[3].GetComponent<UpdatePlayerTimer>();
+        if (contrlr.playerObjects.Count > 2)
+        {
+            timr1.myPlayerName = contrlr.playerName.text;
+            timr2.myPlayerName = contrlr.playerName2.text;
+            timr3.myPlayerName = contrlr.playerName3.text;
+            timr4.myPlayerName = contrlr.playerName4.text;
+        }
+        else
+        {
+            timr1.myPlayerName = contrlr.playerName.text;
+            timr3.myPlayerName = contrlr.playerName3.text;
+        }
+    }
+
+    [PunRPC]
+    public void SetAliveState(bool state)
+    {
+        iamalive = state;
+    }
+
+    public void SetCurrentPlayerIndex(int index)
+    {
+        FindObjectOfType<GameGUIController>().SetCurrentPlayerIndexDav(index);
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus == false)
+        {
+            IamBackRPC();
+        }
+        Debug.LogWarning("On Application pause: " + pauseStatus);
+    }
+
+    void IamBackRPC()
+    {
+        view.RPC("SomeoneISBack", PhotonTargets.Others);
+    }
+
+    [PunRPC]
+    public void SomeoneISBack()
+    {
+        Debug.LogWarning("Someone is back");
+        SendTurns();
+    }
+
+    void SendTurns()
+    {
+        GameGUIController contrlr = FindObjectOfType<GameGUIController>();
+        UpdatePlayerTimer timr1 = contrlr.PlayersTimers[0].GetComponent<UpdatePlayerTimer>();
+        UpdatePlayerTimer timr2 = contrlr.PlayersTimers[1].GetComponent<UpdatePlayerTimer>();
+        UpdatePlayerTimer timr3 = contrlr.PlayersTimers[2].GetComponent<UpdatePlayerTimer>();
+        UpdatePlayerTimer timr4 = contrlr.PlayersTimers[3].GetComponent<UpdatePlayerTimer>();
+        Debug.LogWarning("Send Turns");
+        int currentIndex = FindObjectOfType<GameGUIController>().GetCurrentPlayerIndex();
+        view.RPC("SendTurns", PhotonTargets.Others, timr1.turnCount, timr2.turnCount, timr3.turnCount, timr4.turnCount, timr1.myPlayerName, timr2.myPlayerName, timr3.myPlayerName, timr4.myPlayerName, currentIndex);
+
+    }
+    [PunRPC]
+    public void SendTurns(int one, int two, int three, int four, string id1, string id2, string id3, string id4, int currentPlayerIndex)
+    {
+        Debug.LogWarning("Send Turns with params");
+        //for (int i = 0; i < 4; i++)
+        //{
+        if (FindObjectOfType<GameGUIController>().playerObjects.Count < 3)
+        {
+            FindObjectOfType<GameGUIController>().PlayersTimers[2].GetComponent<UpdatePlayerTimer>().SetTurn(one, id1);
+            //FindObjectOfType<GameGUIController>().PlayersTimers[i].GetComponent<UpdatePlayerTimer>().SetTurn(two, id2);
+            FindObjectOfType<GameGUIController>().PlayersTimers[0].GetComponent<UpdatePlayerTimer>().SetTurn(three, id3);
+            //FindObjectOfType<GameGUIController>().PlayersTimers[i].GetComponent<UpdatePlayerTimer>().SetTurn(four, id4);
+            //}
+        }
+        SetCurrentPlayerIndex(currentPlayerIndex);
+    }
+}
---
+++ Ludo Olympic Local/Assets/Scripts/LudoCashScript/RefferalCodeScript.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using LitJson;
+using System.IO;
+using SimpleJSON;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class RefferalCodeScript : MonoBehaviour
+{
+
+    [Header("Refferal Attribute")]
+
+    public string refferalCodeURL;
+    public InputField _referalCode;
+
+
+    public Text popupText;
+    public GameObject popupPanel;
+    public GameObject loadingPanel;
+    public GameObject friendSharePanel;
+
+    public Text errortext;
+    string status;
+
+    string refCodePP = "FriendRefCodePP";
+
+    private string results;
+    public string Results
+    {
+        get
+        {
+            return results;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public GameObject inviteFriendPanel;
+
+    public void OnInviteFriendEnabled()
+    {
+        if (PlayerPrefs.GetString(refCodePP) == "")
+        {
+            _referalCode.text = PlayerPrefs.GetString(refCodePP);
+            _referalCode.interactable = true;
+            inviteFriendPanel.SetActive(true);
+        }
+        else
+        {
+            _referalCode.text = PlayerPrefs.GetString(refCodePP);
+            _referalCode.interactable = false;
+            inviteFriendPanel.SetActive(true);
+        }
+    }
+
+    public void OnClaimPrize()
+    {
+        if (string.IsNullOrEmpty(_referalCode.text))
+        {
+            errortext.text = "RefferalCode Can't be Blank";
+            StopCoroutine(ErrorClose());
+            StartCoroutine(ErrorClose());
+            return;
+        }
+        WWWForm form = new WWWForm();
+        form.AddField("user_id", GameManager.Uid);
+        form.AddField("refer_code", _referalCode.text);
+        Debug.Log("Code" + _referalCode.text);
+        WWW w = new WWW(refferalCodeURL, form);
+        StartCoroutine(ClaimPrize(w));
+    }
+
+    IEnumerator ClaimPrize(WWW w)
+    {
+        yield return w;
+        print("responce=" + w.text);
+        loadingPanel.SetActive(true);
+        if (w.error == null)
+        {
+            string msg = w.text;
+            msg = msg.Replace("{", "");
+            msg = msg.Replace("}", "");
+            msg = msg.Replace("[", "");
+            msg = msg.Replace("]", "");
+            msg = msg.Replace(@"""", string.Empty);
+            results = GetDataValue(msg, "message:");
+            status = GetDataValue(msg, "status:");
+            Debug.Log(results);
+            Debug.Log(status);
+            if (status == "True")
+            {
+                Debug.Log("this is perfect..!!!");
+                GameManager.Instance.playfabManager.GetReferCode();
+                loadingPanel.SetActive(false);
+                popupPanel.SetActive(true);
+                PlayerPrefs.SetString(refCodePP, _referalCode.text);
+                _referalCode.interactable = false;
+                StartCoroutine(popUpPanel());
+                FindObjectOfType<OfflineOnlineApiScript>().OnlineCash();
+                //_referalCode.text = "";
+                friendSharePanel.SetActive(false);
+            }
+            if (status == "False")
+            {
+                //_referalCode.text = "";
+                loadingPanel.SetActive(false);
+                popupText.text = results;
+                popupPanel.SetActive(true);
+                StartCoroutine(popUpPanel());
+            }
+        }
+    }
+    string GetDataValue(string data, string index)
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        if (value.Contains(","))
+            value = value.Remove(value.IndexOf(","));
+
+        return value;
+    }
+    IEnumerator ErrorClose()
+    {
+        yield return new WaitForSeconds(2f);
+        errortext.text = "";
+        yield return null;
+    }
+    IEnumerator popUpPanel()
+    {
+        yield return new WaitForSeconds(0.5f);
+        popupPanel.SetActive(false);
+    }
+}
